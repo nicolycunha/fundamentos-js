08e1150bd501dceec95b1ec5673e1d7cccfd26d6
nicolycunha <nicolyoliveiracunha.2001@gmail.com>
2022-11-29 08:57:53 -0300

    exemplos da aula 1

---
+++ type-number.js
@@ -0,0 +1,15 @@
+const meuNumero = 3
+
+const primeiroNumero = 1
+const segundoNumero = 2
+
+const operacaoMatematica = primeiroNumero * segundoNumero
+
+console.log(operacaoMatematica)
+
+const numeroPontoFlutuante = 3.3
+const numeroPontoFlutuanteSemZero = 0.5
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante
+
+console.log(novaOperacao)
