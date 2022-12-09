3f55d95768fb5988dab3b468771943588f3c3216
nicolycunha <nicolyoliveiracunha.2001@gmail.com>
2022-12-09 09:08:41 -0300

    declaração de variáveis e conversões

---
+++ conversions.js
@@ -0,0 +1,7 @@
+const numero = 456
+const numeroString = '456'
+
+console.log(numero == numeroString)
+console.log(numero === numeroString)
+console.log(numero + numeroString)
+console.log(numero + Number(numeroString))
---
+++ type-truthy-false.js
@@ -0,0 +1,19 @@
+const usuarioLogado = true
+const contaPaga = false
+
+console.log(0 == false)
+console.log('' == false)
+console.log(1 == true)
+
+//null => vazio ou nada 
+//undefined
+
+let minhaVar;
+let varNull = null;
+
+console.log(minhaVar)
+console.log(typeof minhaVar)
+
+console.log(varNull)
+console.log(typeof varNull)
+
