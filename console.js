b39bd49909afa9dc26b7ee8b96264527a9ddc1a6
nicolycunha <nicolyoliveiracunha.2001@gmail.com>
2022-12-15 07:56:13 -0300

    ternário, template strings, tipagem e console

---
+++ console.js
@@ -0,0 +1,7 @@
+const minhaVar = true
+
+console.log(245)
+console.log('eu sou um texto')
+console.log(minhaVar)
+
+console.error('deu erro!')
---
+++ template-string.js
@@ -0,0 +1,7 @@
+const nome = 'Ni'
+const idade = 2022 - 2001
+const cidadeNascimento = 'São Paulo'
+
+const apresentacao = `Meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeNascimento}.`
+
+console.log(apresentacao)
